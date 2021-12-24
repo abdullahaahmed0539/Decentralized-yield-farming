8dc5db5d5675cffb2b0e29df307359cf96186b88
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2021-12-24 23:49:56 +0500

    blockchain-side complete

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,19 +1,24 @@
-const Tether = artifacts.require("Tether");
-const Reward = artifacts.require("Reward");
-const DecentralizedBank = artifacts.require("DecentralizedBank");
+const RWD = artifacts.require('RWD')
+const Tether = artifacts.require('Tether')
+const DecentralBank = artifacts.require('DecentralBank')
 
 module.exports = async function(deployer, network, accounts) {
-  //deploying contracts
-  await deployer.deploy(Tether);
-  const tether = await Tether.deployed();
-  await deployer.deploy(Reward);
-  const reward = await Reward.deployed();
-  await deployer.deploy(DecentralizedBank, reward.address, tether.address);
-  const decentralized_bank = await DecentralizedBank.deployed();
+  
+  // Deploy Mock Tether Token
+  await deployer.deploy(Tether)
+  const tether = await Tether.deployed()
 
-  //transfer reward tokens to the back
-  await reward.transfer(decentralized_bank.address,"1000000000000000000000000");
+  // Deploy RWD Token
+  await deployer.deploy(RWD)
+  const rwd = await RWD.deployed()
 
-  //transfer tether tokens to investors
-  await tether.transfer(accounts[1], "100000000000000000000");
-};
+  // Deploy DecentralBank
+  await deployer.deploy(DecentralBank, rwd.address, tether.address)
+  const decentralBank = await DecentralBank.deployed()
+
+  // Transfer all tokens to DecentralBank (1 million)
+  await rwd.transfer(decentralBank.address, '1000000000000000000000000')
+
+  // Transfer 100 Mock Tether tokens to investor
+  await tether.transfer(accounts[1], '100000000000000000000')
+}
